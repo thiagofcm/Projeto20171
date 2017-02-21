d408ccc2b95e553012f9cc17edad91fe28353a84
thiagofcm <thiagof1503@gmail.com>
2017-02-21 19:45:35 -0300

    Programando o comportamento da lava com uma corrotina

---
+++ Assets/Scripts/OscilaZ.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class OscilaZ : MonoBehaviour
+{
+
+    public float velocidade;
+    public float min;
+    public float max;
+    public float espera;
+
+    //Use this for inialization
+    void Start()
+    {
+        StartCoroutine(Move(max));
+    }
+
+    IEnumerator Move(float destino)
+    {
+        while (Mathf.Abs(destino - transform.localPosition.z) > 0.2f)
+        {
+            Vector3 direcao = (destino == max) ? Vector3.forward : Vector3.back;
+            Vector3 velocidadeVetorial = direcao * velocidade;
+            transform.localPosition = transform.localPosition + velocidadeVetorial * Time.deltaTime;
+            yield return null;
+        }
+
+        yield return new WaitForSeconds(espera);
+
+        destino = (destino == max) ? min : max;
+        StartCoroutine(Move(destino));
+    }
+
+}
